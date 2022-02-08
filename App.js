27801215f16b6ac67d6cab41bbcd369ff2185cbc
IgorGorlov <igor-skromnik@yandex.ru>
2022-02-08 21:00:58 +0300

    Add-photo-and-video-target

--- App.js
+++ App.js
@@ -2,31 +2,53 @@ import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
-  ViroText,
+  ViroNode,
   ViroConstants,
   ViroARSceneNavigator,
+  ViroARImageMarker,
+  ViroBox,
+  ViroVideo,
 } from '@viro-community/react-viro';
+import targetViroARTrackingTargets from '@viro-community/react-viro/components/AR/ViroARTrackingTargets';
 
 const HelloWorldSceneAR = () => {
   const [text, setText] = useState('Initializing AR...');
 
+  targetViroARTrackingTargets.createTargets({
+    targetOne: {
+      source: require('./resourse/img/flowers.jpg'),
+      physicalWidth: 0.1,
+      orientation: 'Up',
+      type: 'Image',
+    },
+  });
+
   function onInitialized(state, reason) {
     console.log('guncelleme', state, reason);
     if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
+      setText('Игорь!');
     } else if (state === ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
-
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
+      {/*<ViroText
         text={text}
         scale={[0.5, 0.5, 0.5]}
         position={[0, 0, -1]}
         style={styles.helloWorldTextStyle}
-      />
+      />*/}
+
+      <ViroARImageMarker target={'targetOne'}>
+        <ViroVideo
+          source={require('./resourse/video/flower.mp4')}
+          loop={false}
+          position={[0, 0.02, -5]}
+          scale={[0.15, 0.15, 0]}
+        />
+        {/* <ViroBox position={[0, 0.02, 0]} scale={[0.05, 0.05, 0.05]} />*/}
+      </ViroARImageMarker>
     </ViroARScene>
   );
 };
